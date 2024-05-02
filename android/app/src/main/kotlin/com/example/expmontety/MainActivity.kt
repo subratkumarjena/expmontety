eef2b7119c0955801589875a0f22ab6e31875ea9
DESKTOP-RIQEACG\User <subratjena0001@gmail.com>
2024-05-02 19:24:22 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/expmontety/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expmontety
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/onbording.dart
@@ -0,0 +1,56 @@
+import 'package:expmontety/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Onbording extends StatelessWidget {
+  Color myColor = Color(0xFF4752A5);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("Monety"),centerTitle:true ,),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+
+              alignment: Alignment.topLeft,
+
+              child: Image.asset('assets/images/onbordingimg.jpg')),
+
+          Container(
+
+
+              child:Center(
+                  child: Column(
+                    children: [
+                      Text("   Easy way to monitor  ",
+                        style: TextStyle(fontSize: 24,
+                            fontWeight: FontWeight.bold),),
+                      Text("  your expense",
+                        style: TextStyle(fontSize: 24,
+                            fontWeight: FontWeight.bold),),
+                      SizedBox(height: 20,),
+                      Text("Safe Your future by managing your \n              expense right now",
+                        style: TextStyle(fontSize: 15,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.normal),),
+                    ],
+                  )), ),
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton( onPressed: (){
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (cotext){
+          return Home();
+        }));
+
+      },backgroundColor: myColor,
+        child: Icon(Icons.arrow_forward,color: Colors.white,),
+
+      ),
+    );
+  }
+
+}
