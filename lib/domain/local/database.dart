999b899a0560c507e48c120c65a44c50a28d6dd1
DESKTOP-RIQEACG\User <subratjena0001@gmail.com>
2024-05-04 19:03:06 +0530

    user resistrationand login

--- lib/domain/local/database.dart
+++ lib/domain/local/database.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 
 class AppDatabase{
@@ -33,8 +36,8 @@ class AppDatabase{
 
     return await openDatabase(actualpath, version: 1,onCreate: (db, version){
       // table create
-      db.execute("create table note ( user_id integer primary key autoincrement, user_phone text,"
-          " user_email text,user_name text,user_pass text )");
+      db.execute("create table user ( user_id integer primary key autoincrement, user_phone text, user_email text unique ,user_name text,user_pass text )");
+      db.execute("create table note ( exp_id integer primary key autoincrement,user_id integer, exp_title text, exp_desc text, exp_amount text, exp_date&time text  )");
 
 
     });
@@ -50,8 +53,8 @@ class AppDatabase{
     var cdb = await getDb();
     cdb.insert( "note" , {
 
-      "note_title": ntitle ,
-      "note_desc" : ndesc
+      "exp_title": ntitle ,
+      "exp_desc" : ndesc
     });
 
   }
@@ -66,19 +69,71 @@ class AppDatabase{
 
   void remove(int id) async {
     var cdb = await getDb();
-    cdb.delete("note", where: "note_id = ?", whereArgs: [id]);
+    cdb.delete("note", where: " exp_id = ?", whereArgs: [id]);
   }
   void updateda({ required String ntitle, required String ndesc, required id}) async{
 
     var cdb = await getDb();
     cdb.update("note", {
 
-      "note_title": ntitle ,
-      "note_desc" : ndesc,
-
-    },where: "note_id = ?", whereArgs: [id],);
+      "exp_title": ntitle ,
+      "exp_desc" : ndesc,
+    },where: "exp_id = ?", whereArgs: [id],);
 
   }
+  /////
+  /////user signup
+  Future<bool> addNewUser({required String uPhone,required String uEmail,required String uName,required String uPass }) async{
+    var cdb = await getDb();
+    bool haveaccount = await emailexit(uEmail);
+    bool accountcreated  = false;
+
+    if(!haveaccount){
+
+   var rowsEffected  =  await     cdb.insert("user",{
+        "user_phone": uPhone ,
+        "user_email" : uEmail ,
+        "user_name" : uName ,
+        "user_pass" : uPass ,
+      });
+     accountcreated = rowsEffected>0;
+    }
+
+    return accountcreated;
+  }
+
+  Future<bool>  emailexit(String email) async{
+    var cdb = await getDb();
+      var mData  = await cdb.query("user", where: "user_email = ?", whereArgs: [email]);
+      return mData.isNotEmpty;
+  }
+
+  /// user login
+   Future<bool> authuser({ required String email, required String pass}) async{
+
+     var cdb = await getDb();
+     List<Map<String, dynamic>> mData  = await cdb.query("user", where: "user_email = ? AND user_pass = ?" , whereArgs: [email,pass]);
+
+
+
+     if(mData.isNotEmpty){
+       setUID( mData);
+
+     }
+
+     return mData.isNotEmpty;
+
+   }
+
+   void getUID(){
+
+   }
+   void setUID(mData) async{
+
+     var prefs = await SharedPreferences.getInstance();
+     prefs.setInt("uId",mData[0]["user_id"] );
+
+   }
 
 
 }
